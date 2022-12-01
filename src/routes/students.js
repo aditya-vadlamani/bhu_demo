aec347e9e323c30d6a7a84072aef0245ab5efa06
aditya-vadlamani <aditya.vadlamani@yahoo.in>
2022-12-01 17:07:56 +0530

    put and delete

--- src/routes/students.js
+++ src/routes/students.js
@@ -65,4 +65,67 @@ router.post('/add', (req, res, next) =>{
     })
 })
 
+
+router.put('/edit/:name', (req, res, next) => {
+    let connection = mysql.createConnection({
+        host: "127.0.0.1",
+        port: "8889",
+        database: "bhu_demo",
+        user: "bhu_user",
+        password: "test@123"
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log("Error occued " + err);
+        }
+
+
+        console.log("Connected to the database");
+
+        var name = req.params.name;
+        var rollno = req.body.rollno;
+
+        var updateQuery = "UPDATE student_list SET rollno = " + rollno + " WHERE name = " + connection.escape(name);
+
+        connection.query(updateQuery, (err) => {
+            if(err){
+                console.log("error occured " + err);
+            }
+
+            res.send({"message": "student rollno updated"});
+            connection.end();
+        })
+    })
+});
+
+
+router.delete("/delete/:name", (req, res, next) => {
+    let connection = mysql.createConnection({
+        host: "127.0.0.1",
+        port: "8889",
+        database: "bhu_demo",
+        user: "bhu_user",
+        password: "test@123"
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log("error is " + err);
+        }
+
+        var name = req.params.name;
+
+        var deleteQuery = "DELETE FROM student_list WHERE name = " + connection.escape(name);
+
+        connection.query(deleteQuery, (err) => {
+            if(err){
+                console.log(err);
+            }
+
+            res.send({"message": "deleted"})
+            connection.end();
+        })
+    })
+})
 module.exports = router;
